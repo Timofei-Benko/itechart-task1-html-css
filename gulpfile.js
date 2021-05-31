6cc65e872f65c1ecf22c95c58393584006cfbb7c
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-05-31 18:30:20 +0300

    feat: add url resolver

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 const gulp = require("gulp");
 const del = require("del");
 const sass = require("gulp-sass");
+const sourcemaps = require('gulp-sourcemaps');
+const gulpResolveUrl = require('gulp-resolve-url');
 
 gulp.task('styles', () => {
-    return gulp.src('./src/style/scss/**/*.scss')
+    return gulp.src('./src/style/scss/**/*.scss', { base: './src'})
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
+        .pipe(gulpResolveUrl())
         .pipe(gulp.dest('./src/style/css'));
 });
 
