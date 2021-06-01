cf20b2b6564d2fac0a39f985a14edf923ff1ba0f
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-06-01 12:15:34 +0300

    feat: add webserver

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 const gulp = require("gulp");
 const del = require("del");
 const sass = require("gulp-sass");
-// const sourcemaps = require('gulp-sourcemaps');
+const connect = require("gulp-connect");
+// // const sourcemaps = require('gulp-sourcemaps');
 // const gulpResolveUrl = require('gulp-resolve-url');
 
 gulp.task('styles', () => {
     return gulp.src('./src/style/scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./src/style/css'));
+        .pipe(gulp.dest('./src/style/css'))
+        .pipe(connect.reload());
 });
 
 gulp.task('clean', () => {
@@ -22,4 +24,11 @@ gulp.task('watch', () => {
     });
 });
 
-gulp.task('default', gulp.series(['clean', 'styles']));
+gulp.task('webserver', function() {
+    connect.server({
+        root: 'src',
+        livereload: true,
+    });
+});
+
+gulp.task('default', gulp.series(['webserver', 'watch']));
